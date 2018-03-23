38aca4756c54c6c6341ab07548f8cc7366e12be9
Sandeep Munavalli <smunaval@mathworks.com>
2018-03-23 09:56:11 -0400

    adding a matlab file

---
+++ testMatlab.m
@@ -0,0 +1,11 @@
+% @todo create an issue for testing
+% @body check if that is working
+
+function c = addMe(a,b)
+% ADDME addign two value
+% C = ADDME(A) adds A to itself
+% C = ADDME(A,B) adds A and B togather
+% See also SUM, PLUS
+
+	c = a + b
+end
